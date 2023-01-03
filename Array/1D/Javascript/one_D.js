4a223b26e6da22c66364b75af761e754568bb75d
Titus Kiplagat <tituskiplagat210@gmail.com>
2023-01-03 13:08:41 +0300

    Initializing array Prompt user to enter elements of array loop through array and prompt user to enter items store items in array loop through items and print with index

--- Array/1D/Javascript/one_D.js
+++ Array/1D/Javascript/one_D.js
@@ -1,7 +1,17 @@
-// initialize array
-
+//initialize array
 var array = [];
 
+// prompting user to enter number of elements in an array
 
-var noOfElements = Number(prompt("Please Enter no of elements in array"));
-console.log(noOfElements)
+const num = prompt('Enter number of Elements in Array: ');
+
+// loop through number of element and add them to array
+for (var i = 0; i <= num; i++) {
+    element = prompt('Enter element to store in array: ');
+    array[i] = element;
+}
+
+// loop through elements in a array and print
+for( var i = 0; i < array.length; i++){
+    console.log(`The element at position ${i} is ${array[i]}`);
+}
