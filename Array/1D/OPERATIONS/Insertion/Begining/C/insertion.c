fb4158cc57da066e998fa3b6e8c18957155a5198
Titus Kiplagat <tituskiplagat210@gmail.com>
2023-01-10 17:51:29 +0300

    Insertion array

--- Array/1D/OPERATIONS/Insertion/Begining/C/insertion.c
+++ Array/1D/OPERATIONS/Insertion/Begining/C/insertion.c
@@ -5,7 +5,7 @@
 void main()
 {
     // initialize array, position, i
-    int array[MAX] = {1666666, 2, 3, 4, 5}, element;
+    int array[MAX] = {1, 2, 3, 4, 5}, element;
     int i = 0;
     int pos = 0; // since beginning is index 0
 
@@ -16,11 +16,10 @@ void main()
     // loop through array and shift elements to last index
     for (i = MAX; i > pos; i--)
     {
-        array[i] = array[i - 1];
-
-        // insert element
-        array[pos] = element;
+        array[i] = array[i + 1];
     }
+    // insert element
+    array[pos] = element;
 
     // print the array which is now size +1
 
