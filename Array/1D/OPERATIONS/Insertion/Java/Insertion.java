6685d042e25fe023c3bde4e0e06612eb9b0cb4ff
Titus Kiplagat <tituskiplagat210@gmail.com>
2023-05-01 05:49:23 +0300

    CONT: Insertion Implemented insertion at given index of an array Added method to check if array still has space

--- Array/1D/OPERATIONS/Insertion/Java/Insertion.java
+++ Array/1D/OPERATIONS/Insertion/Java/Insertion.java
@@ -1,21 +1,19 @@
 import java.util.Scanner;
 
 class Insertion {
-    static Scanner input = new Scanner(System.in);
-    static int i;
-    static int choice;
-    static int[] students_id = new int[10];
+    Scanner input = new Scanner(System.in);
+    int i, choice, pos;
+    int[] students_id = new int[10];
 
     /* Method to get the number of students */
-    static int getStudentsNumber() {
+    int getStudentsNumber() {
         System.out.println("Enter number of students Id to enter: 1-10 ");
         int no_students = input.nextInt();
 
         return no_students;
     }
 
-    int insertAtBeginning() {
-        // check if array is full
+    int displayInsertionMessage() {
         if (isArrayFull() == -1) {
             System.out.println("Sorry We cant insert more elements in the array:");
             System.out.println("1. Delete some elements in the array\n 2. Quit");
@@ -25,9 +23,38 @@ class Insertion {
                 case 1:
                     deleteElements();
                 case 2:
-                    return 0;
+                    exit();
+            }
         }
+        return 1;
+    }
 
+    int[] insertAtBeginning() {
+        // check if array is full
+        if (displayInsertionMessage() == 1) {
+            System.out.println("Enter element to insert");
+            choice = input.nextInt();
+            System.out.println("Enter position to insert");
+            pos = input.nextInt();
+            for (i = students_id.length - 1; i == students_id[0]; i--) {
+                students_id[i + 1] = students_id[i];
+            }
+            students_id[0] = choice;
+        }
+        return students_id;
+
+    }
+
+    int[] insertAtGivenPos() {
+        if (displayInsertionMessage() == 1) {
+            System.out.println("Enter element to insert");
+            choice = input.nextInt();
+            for (i = students_id.length - 1; i == students_id[0]; i--) {
+                students_id[i + 1] = students_id[i];
+            }
+            students_id[0] = choice;
+        }
+        return students_id;
     }
 
     int isArrayFull() {
@@ -45,34 +72,47 @@ class Insertion {
             return 1;
     }
 
-    
+    /* Delete elements in an array */
+    int deleteElements() {
+        System.out.println("Enter position to delete ");
+        choice = input.nextInt();
+
+        return -1;
+    }
+
+    int exit() {
+        return 0;
+    }
+
     public static void main(String[] args) {
         // declare array
-     
-        int no_students = getStudentsNumber();
-      
+        Insertion instance = new Insertion();
+
+        int no_students = instance.getStudentsNumber();
 
         // check if input is greater than array size
-        if (no_students > students_id.length) {
+        if (no_students > instance.students_id.length) {
             System.out.println("Number entered is greater than 10");
-            getStudentsNumber();
+            instance.getStudentsNumber();
         }
 
         // loop to insert elements into and array
-        for (i = 1; i <= no_students; i++) {
-            System.out.println("Enter student " + (i) + " id");
-            students_id[i - 1] = input.nextInt();
+        for (instance.i = 1; instance.i <= no_students; instance.i++) {
+            System.out.println("Enter student " + (instance.i) + " id");
+            instance.students_id[instance.i - 1] = instance.input.nextInt();
         }
 
         // print students id in the array
-        for (i = 1; i <= no_students; i++) {
-            System.out.println(students_id[i - 1]);
+        for (instance.i = 1; instance.i <= no_students; instance.i++) {
+            System.out.println(instance.students_id[instance.i - 1]);
         }
 
         /*
          * insertion in array
          * insert at begining
          */
+        instance.insertAtBeginning();
+        instance.insertAtGivenPos();
 
     }
 }
