1658fddd603c18eed841be62472bd93d973239ec
Titus Kiplagat <tituskiplagat210@gmail.com>
2023-05-01 05:15:31 +0300

    CONT: Insertion of element in java Added a function to insert elements Checked if array is full Implemented a  method to check if array is empty Created a call to deleteElements which is not yet declared

--- Array/1D/OPERATIONS/Insertion/Java/Insertion.java
+++ Array/1D/OPERATIONS/Insertion/Java/Insertion.java
@@ -1,23 +1,56 @@
 import java.util.Scanner;
 
 class Insertion {
+    static Scanner input = new Scanner(System.in);
+    static int i;
+    static int choice;
+    static int[] students_id = new int[10];
 
     /* Method to get the number of students */
     static int getStudentsNumber() {
-        Scanner input = new Scanner(System.in);
         System.out.println("Enter number of students Id to enter: 1-10 ");
         int no_students = input.nextInt();
 
         return no_students;
     }
 
+    int insertAtBeginning() {
+        // check if array is full
+        if (isArrayFull() == -1) {
+            System.out.println("Sorry We cant insert more elements in the array:");
+            System.out.println("1. Delete some elements in the array\n 2. Quit");
+            choice = input.nextInt();
+
+            switch (choice) {
+                case 1:
+                    deleteElements();
+                case 2:
+                    return 0;
+        }
+
+    }
+
+    int isArrayFull() {
+        if (students_id.length == 10)
+            return -1;
+        else
+            return 1;
+    }
+
+    // check if arrays id empty
+    int isArrayEmpty() {
+        if (students_id.length == 0 || students_id == null)
+            return -1;
+        else
+            return 1;
+    }
+
+    
     public static void main(String[] args) {
         // declare array
-        int[] students_id = new int[10];
+     
         int no_students = getStudentsNumber();
-        int i;
-
-        Scanner input = new Scanner(System.in);
+      
 
         // check if input is greater than array size
         if (no_students > students_id.length) {
@@ -35,6 +68,11 @@ class Insertion {
         for (i = 1; i <= no_students; i++) {
             System.out.println(students_id[i - 1]);
         }
+
+        /*
+         * insertion in array
+         * insert at begining
+         */
 
     }
 }
