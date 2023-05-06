226cf92172b30ff4cc297adc0c725bb23f5f97da
Titus Kiplagat <tituskiplagat210@gmail.com>
2023-05-06 21:48:57 +0300

    Complete code to insert Element in Stack Implemented function to print element at end of function

--- Stacks/C++/arrayImplementation.cpp
+++ Stacks/C++/arrayImplementation.cpp
@@ -5,38 +5,56 @@ using namespace std;
 
 // functions
 void getOperation(int *choice);
+int isFull(int *top);
+int isEmpty(int *top);
+void push(int *stack, int *top, int *data, int *choice);
+void pop(int *stack, int *top, int *data);
+void printStack(int *stack, int *top);
 
+/**
+ * @brief
+ *
+ */
 int main()
 {
   // declare stack
   int stack[MAX];
-  int choice, top, data;
+  int choice, top = -1, data;
 
-  /**
-   * @brief Ask user for stack operations
-   *
-   */
+  // @brief Ask user for stack operations
   getOperation(&choice);
-  cout << choice << endl;
 
   switch (choice)
   {
   case 1:
     isFull(&top);
+    break;
   case 2:
     isEmpty(&top);
+    break;
   case 3:
-    push(stack, &top, &data);
+    push(stack, &top, &data, &choice);
+    break;
   case 4:
     pop(stack, &top, &data);
+    break;
   case 5:
-    printStack(stack);
+    printStack(stack, &top);
+    break;
   default:
     cout << "ERROR: Enter a valid choice";
+    break;
   }
+  printStack(stack, &top);
+
+  return 0;
 }
 
-// get operation
+/**
+ * @brief Get the Operation object
+ *
+ * @param choice
+ */
 void getOperation(int *choice)
 {
   cout << "What stack operation do you want to perform?" << endl;
@@ -50,49 +68,106 @@ void getOperation(int *choice)
 
 /**
  * @brief it checks if stack is full bu comparing top element with the maximum size of stack -1 since array indexing starts from 0
- * 
+ *
  * @param top this is the top element of stack
  * @return int -1 if stack is full and 1 if the stack is not full
  */
 
-int isFull(int *top){
+int isFull(int *top)
+{
 
-  if(*top == MAX -1)
+  if (*top == MAX - 1)
+  {
+    cout << "Overflow occurred " << endl;
     return -1;
+  }
+  cout << "Stack not full" << endl;
   return 1;
-
 }
 
 /**
- * @brief isEmpty function checks if stack is empty by comparing the top element od stack with -1 which is initial value or 
+ * @brief isEmpty function checks if stack is empty by comparing the top element od stack with -1 which is initial value or
  * top is less than 0 which is obviously a negative number
- * 
+ *
  * @param top this is the topmost element in the stack
  * @return int -1 if stack is empty and 1 if stack contains som elements
  */
-int isEmpty(int *top){
+int isEmpty(int *top)
+{
 
-  if(*top == -1 || *top < 0)
+  if (*top == -1 || *top < 0)
+  {
+    cout << "Underflow detected" << endl;
     return -1;
+  }
+  cout << "Stack is not empty" << endl;
   return 1;
-
 }
 
-
-void push(int *stack, int *top, int *data){
+/**
+ * @brief
+ *
+ * @param stack
+ * @param top
+ * @param data
+ */
+void push(int *stack, int *top, int *data, int *choice)
+{
   // check if stack is full
-  if(isFull == -1){
-    cout<<"Sorry we cant insert more elements to the stack " << endl;
+  if ((isFull(top) == -1))
+  {
+    cout << "Sorry we cant insert more elements to the stack " << endl;
+    return;
   }
   cout << "Enter element to insert" << endl;
   cin >> *data;
   // increment the top of stack
-  top++;
-  stack[top] == data;
+  (*top)++;
+  stack[*top] = *data;
+
 }
 
+/**
+ * @brief
+ *
+ * @param stack
+ * @param top
+ * @param data
+ */
+void pop(int *stack, int *top, int *data)
+{
+  // check if stack is empty
+  if ((isEmpty(top) == -1))
+  {
+    cout << " Cant remove element from an empty array" << endl;
+    return;
+  }
+  *data = stack[*top];
+  (*top)--;
+  cout << "Element " << *data << " popped from the stack" << endl;
+  return;
+}
 
-
-void pop(int *stack, int *top, int *data){
-  
+/**
+ * @brief
+ *
+ * @param stack
+ * @param top
+ */
+void printStack(int *stack, int *top)
+{
+  // check if stack is empty
+  if ((isEmpty(top) == -1))
+  {
+    cout << " Cant Print elements in an empty array" << endl;
+    return;
+  }
+  else
+  {
+    for (int i = 0; i <= *top; i++)
+    {
+      cout << "Element " << i + 1 << " in stack is: " << stack[i] << endl;
+    }
+    return;
+  }
 }
