fd626a3bbb5f68085370ede4966070f6508d745c
Titus Kiplagat <tituskiplagat210@gmail.com>
2023-05-14 06:01:10 +0300

    feat: checks if queue is full - it checks if front  is in first position and rear is in last position of queue - checks if rear is equal to front -1 modulo the maximum size -1 - checks if front is equal to rear -1
    returns true if any of above condition is met
    returns false if none of the above conditions are met

--- Queue/C++/circlularQueue.cpp
+++ Queue/C++/circlularQueue.cpp
@@ -3,18 +3,34 @@ using namespace std;
 
 #define MAX_QUEUE_SIZE 5
 
-class CircularQueue{
-    private:
-        int queue[MAX_QUEUE_SIZE], front, rear;
-    public:
+class CircularQueue
+{
+private:
+    int queue[MAX_QUEUE_SIZE], front, rear;
+
+public:
     /**
      * @brief Construct a new Circular Queue object with initial values of front and rear to -1
-     * 
+     *
      */
-    CircularQueue(){
-        front= -1;
+    CircularQueue()
+    {
+        front = -1;
         rear = -1;
     }
 
+    /**
+     * @brief checks if the queue is full
+     *
+     * @return true if queue is full
+     * @return false  if queue is not full
+     */
+    bool isFull()
+    {
+        if ((front == 0 && rear == MAX_QUEUE_SIZE - 1) || (rear == (front - 1) % (MAX_QUEUE_SIZE - 1)) || (front == rear + 1))
+            return true;
+        else
+            return false;
+    }
 
 };
