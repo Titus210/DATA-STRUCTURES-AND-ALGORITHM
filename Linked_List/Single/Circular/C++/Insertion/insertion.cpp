8597e55b7f1a50bb188616e62241d8163d36f5c7
Titus Kiplagat <tituskiplagat210@gmail.com>
2023-05-24 12:18:33 +0300

    feat: insertion at the nd -params: last node and data to insert - updated readme.md for the description of the files

--- Linked_List/Single/Circular/C++/Insertion/insertion.cpp
+++ Linked_List/Single/Circular/C++/Insertion/insertion.cpp
@@ -68,6 +68,57 @@ struct Node* beginning_insertion(struct Node* last, int data){
 }
 
 /**
+ * @brief Inserts node at the end of the linked list
+ * 
+ * @param last: - This is the pointer to the last element in the list
+ * @param data: - The data to be inserted into the linked list
+ * @return struct Node* 
+ */
+struct Node* end_insertion(struct Node* last, int data){
+    // check if the list is empty
+    if(last == NULL){
+        // allocate memory for the new node
+        struct Node* new_node = (struct Node*)malloc(sizeof(struct Node));
+        if(new_node == NULL){
+            printf("Memory allocation failed.\n");
+            return last;
+        }
+
+        // assign data to the new node
+        new_node->data = data;
+
+        // create link
+        new_node->next = new_node;
+
+        // update last to point to new node
+        last = new_node;
+
+        return last;
+    }
+
+    // allocate memory for the new node
+    struct Node* new_node = (struct Node*)malloc(sizeof(struct Node));
+    if(new_node == NULL){
+        printf("Memory allocation failed.\n");
+        return last;
+    }
+
+    // assign data to the new node
+    new_node->data = data;
+
+    // link new node with the first node
+    new_node->next = last->next;
+
+    // update last to point to new node
+    last->next = new_node;
+
+    // make new node the last node
+    last = new_node;
+
+    return last;
+}
+
+/**
  * @brief entry point for the insertion code
  * 
  * @return int 
@@ -81,5 +132,30 @@ int main(){
 
     print_list(last);
 
+
+    last = end_insertion(last, 12);
+    if(last == NULL)
+        return 1;
+
+    last = end_insertion(last, 34);
+    if(last == NULL)
+        return 1;
+
+    last = end_insertion(last, 56);
+    if(last == NULL)
+        return 1;
+
+    print_list(last);
+
+    // Free allocated memory
+    struct Node* current = last->next;
+    while(current != last){
+        struct Node* temp = current;
+        current = current->next;
+        free(temp);
+    }
+    free(last);
+
     return 0;
+
 }
